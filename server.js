b63b23ced4e5c62a330fda3b5102e73d6f2e22a3
Eaug2 <erik.augenzucker@gmail.com>
2018-05-03 15:21:40 -0400

    added heroku monogo connection

--- server.js
+++ server.js
@@ -7,13 +7,16 @@ let app = express();
 var mongoose = require('mongoose');
 
 mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://localhost/mean-angular5', { promiseLibrary: require('bluebird') })
+mongoose.connect('mongodb://localhost/subtracked', { promiseLibrary: require('bluebird') })
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
 
 
 app.use(express.static(__dirname + "/dist"));
 app.use('/Github', github);
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/subtracked";
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
