ae21b8962e7c08f319c011c628389211fa3755b2
Eaug2 <erik.augenzucker@gmail.com>
2018-05-03 15:27:06 -0400

    trying to get mongo and heroku to work

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
 var github = require('./routes/github');
-
+var mongoose = require('mongoose');
 let app = express();
 
-var mongoose = require('mongoose');
 
-mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://localhost/subtracked', { promiseLibrary: require('bluebird') })
-  .then(() =>  console.log('connection succesful'))
-  .catch((err) => console.error(err));
+
+// mongoose.Promise = require('bluebird');
+// mongoose.connect('mongodb://localhost/subtracked', { promiseLibrary: require('bluebird') })
+//   .then(() =>  console.log('connection succesful'))
+//   .catch((err) => console.error(err));
 
 
 app.use(express.static(__dirname + "/dist"));
@@ -17,6 +17,11 @@ app.use('/Github', github);
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/subtracked";
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
