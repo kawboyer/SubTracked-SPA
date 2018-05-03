999ec2a75228a0429fabb33b03a56927530e79fc
taddes <taddes.korris@gmail.com>
2018-05-03 14:05:08 -0400

    added firebase, nav and signup components

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
 <template>
   <div id="app">
-    <Footer></Footer>
-    <img src="./assets/logo.png">
+    <Navbar></Navbar>
+   
     <router-view/>
+     <Footer></Footer>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/layout/Navbar'
 import Footer from '@/components/layout/Footer'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar,
+    Footer
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
