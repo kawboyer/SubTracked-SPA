da76d9441ba5e787b6f6cfb0ee358e6eefae3d17
Klara Boyer <kawboyer@gmail.com>
2018-05-04 14:11:07 -0400

    rendering pie chart with subscription.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <Navbar></Navbar>
-    <canvas id="planet-chart"></canvas>
-    <PlantChart></PlantChart>
+
     <router-view/>
      <Footer></Footer>
   </div>
@@ -11,14 +10,11 @@
 <script>
 import Navbar from '@/components/layout/Navbar'
 import Footer from '@/components/layout/Footer'
-import Chart from 'chart.js'
-import planetChartData from '@/components/home/chart-data.js'
 export default {
   name: 'App',
   components: {
     Navbar,
-    Footer,
-    planetChartData,
+    Footer
   }
 }
 </script>
